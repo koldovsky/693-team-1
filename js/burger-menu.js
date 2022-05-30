a2841fa8fa62350984716a41bf8c51adf079925e
andrusb21 <andrusyshyn1996@gmail.com>
2022-05-30 12:19:39 +0000

    Added a working slider burger

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,5 +1,12 @@
-
 const burger = document.querySelector(".header-nav__burger")
-burger.addEventListener("click",function (){
+const headerNav = document.querySelector(".header_nav")
+const headerNavLinks = document.querySelector(".header_nav-links")
+const headerNavLinksA = document.querySelector(".header__top-navigation-links")
+const body = document.querySelector("body")
+burger.addEventListener("click", function () {
     burger.classList.toggle("active");
+    headerNav.classList.toggle("active");
+    headerNavLinks.classList.toggle("active");
+    headerNavLinksA.classList.toggle("active")
+    body.classList.toggle("lock")
 })
